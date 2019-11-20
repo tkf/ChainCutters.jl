f58db20fb7a9bd294257181744982d92b6e96b3e
Takafumi Arakaki <aka.tkf@gmail.com>
2019-11-19 22:39:48 -0800

    Test with Zygote 0.3 (#13)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
 module TestChainCutters
 using Test
+using Zygote
+
+if lowercase(get(ENV, "CI", "false")) == "true" && !isdefined(Zygote, :pullback)
+    @info "Testing with Zygote 0.3 in CI. Monkey patching it for test..."
+    @eval Zygote pullback = forward
+end
 
 @testset "$file" for file in sort([file for file in readdir(@__DIR__) if
                                    match(r"^test_.*\.jl$", file) !== nothing])
